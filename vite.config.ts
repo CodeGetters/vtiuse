873f2c823e96850e5cfe850a1b939b16116cc640
Johnson Xin <2775145275@qq.com>
2023-08-22 13:46:02 +0800

    feat: ✨ add unit test - vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig, loadEnv } from "vite";
 import { resolve } from "node:path";
 import vue from "@vitejs/plugin-vue";
@@ -15,7 +17,12 @@ export default ({ mode }) => {
     resolve: {
       alias: {
         "@": resolve(__dirname, "src"),
+        "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.prod.js",
       },
+    },
+    test: {
+      include: ["src/test/*.test.ts"],
+      globals: true,
     },
     build: {
       rollupOptions: {
